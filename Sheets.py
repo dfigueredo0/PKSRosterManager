c50b6554149c7ab0db5b83aa11d7c885a96c9f55
Dylan Figueredo <d.figueredo2104@gmail.com>
2024-05-10 14:16:22 -0500

    Create a Spreadsheet

--- Sheets.py
+++ Sheets.py
@@ -1,5 +1,5 @@
 import os
-from Google import Create_Service
+from PKSRosterManager.Google import Create_Service
 
 CLIENT_SECRET_FILE = 'client_secret.json'
 API_NAME = 'sheets'
@@ -8,3 +8,6 @@ SCOPES = ['https://www.googleapis.com/auth/drive.file']
 
 service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
+# Create a Blank Spreadsheet
+sheets_file1 = service.spreadsheets().create().execute()
+print(sheets_file1)
